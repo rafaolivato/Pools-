dae20eebd0658efee0d17fb26d21e4a4685184ce
rafaolivato <rafaolivato82@gmail.com>
2024-03-01 13:49:28 -0300

    Projeto Django/Python

---
+++ mysite/mysite/urls.py
@@ -0,0 +1,11 @@
+
+from django.contrib import admin
+from django.urls import include, path
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns = [
+    path("polls/", include("polls.urls")),
+    path("admin/", admin.site.urls),
+    path('ckeditor/', include('ckeditor_uploader.urls'))
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
